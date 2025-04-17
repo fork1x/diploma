2e4dbd6ed8aee8e6c5a19a823bbf8f271daa5ba0
forksez <quad1812k@gmail.com>
2025-04-17 16:19:54 +0200

    Полноценная поисковая система

--- goods/utils.py
+++ goods/utils.py
@@ -1,17 +1,18 @@
 from goods.models import Products
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank, SearchHeadline # Для полоценного поиска
 from django.db.models import Q # Импортируем класс Q из модуля django.db.models для создания сложных запросов к базе данных
 
 def q_search(query):
     if query.isdigit() and len(query) <= 3:
         return Products.objects.filter(id=int(query))
-    return Products.objects.filter(description__search=query)
     
-    # keywords = [word for word in query.split() if len(word) > 2] # Разбиваем строку на слова и оставляем только те, которые длиннее 2 символов
 
-    # q_objects = Q() # Создаем объект Q для создания сложных запросов к базе данных
+    vector = SearchVector('description', 'name') # Создаем векторный индекс для полнотекстового поиска по полям description и name
+    query = SearchQuery(query)
 
-    # for token in keywords:
-    #     q_objects |= Q(description__icontains=token) # Добавляем условие поиска по описанию товара
-    #     q_objects |= Q(name__icontains=token)
+    result = Products.objects.annotate(rank=SearchRank(vector, query)).filter(rank__gt=0).order_by('-rank')
+    
+    result = result.annotate(headline=SearchHeadline('name', query, start_sel='<span style="background-color: yellow;">', stop_sel='</span>')) # Добавляем аннотацию headline с выделением найденных слов
+    result = result.annotate(bodyline=SearchHeadline('description', query, start_sel='<span style="background-color: yellow;">', stop_sel='</span>'))
 
-    # return Products.objects.filter(q_objects) # Возвращаем товары, которые соответствуют условиям поиска
+    return result
