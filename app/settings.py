f0b1158be6c1132c8c42cc93dc100e8bf00d356a
forksez <quad1812k@gmail.com>
2025-04-17 20:07:39 +0200

    SAVE

--- app/settings.py
+++ app/settings.py
@@ -142,6 +142,8 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+AUTH_USER_MODEL = 'users.User'  # Модель пользователя
+
 
 INTERNAL_IPS = [
     # ...
--- users/models.py
+++ users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    image = models.ImageField(upload_to='users_images', blank=True, null=True, verbose_name='Изображение')
+
+    class Meta:
+        db_table = 'user'
+        verbose_name = 'пользователя'
+        verbose_name_plural = 'Пользователи'
+    def __str__(self):
+        return self.username
