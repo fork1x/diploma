9a4909247f531709ef0911cc7b9e532b940a433e
forksez <quad1812k@gmail.com>
2025-04-17 15:02:39 +0200

    +PostgreSQL и обновл. поиск

--- app/settings.py
+++ app/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',  # Статические файлы
+    'django.contrib.postgres',  # PostgreSQL
 
     'debug_toolbar',  # Отладка
 
@@ -82,8 +83,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'home',
+        'USER': 'home',
+        'PASSWORD': 'home',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
--- goods/utils.py
+++ goods/utils.py
@@ -4,13 +4,14 @@ from django.db.models import Q # Импортируем класс Q из мод
 def q_search(query):
     if query.isdigit() and len(query) <= 3:
         return Products.objects.filter(id=int(query))
+    return Products.objects.filter(description__search=query)
     
-    keywords = [word for word in query.split() if len(word) > 2] # Разбиваем строку на слова и оставляем только те, которые длиннее 2 символов
+    # keywords = [word for word in query.split() if len(word) > 2] # Разбиваем строку на слова и оставляем только те, которые длиннее 2 символов
 
-    q_objects = Q() # Создаем объект Q для создания сложных запросов к базе данных
+    # q_objects = Q() # Создаем объект Q для создания сложных запросов к базе данных
 
-    for token in keywords:
-        q_objects |= Q(description__icontains=token) # Добавляем условие поиска по описанию товара
-        q_objects |= Q(name__icontains=token)
+    # for token in keywords:
+    #     q_objects |= Q(description__icontains=token) # Добавляем условие поиска по описанию товара
+    #     q_objects |= Q(name__icontains=token)
 
-    return Products.objects.filter(q_objects) # Возвращаем товары, которые соответствуют условиям поиска
+    # return Products.objects.filter(q_objects) # Возвращаем товары, которые соответствуют условиям поиска
