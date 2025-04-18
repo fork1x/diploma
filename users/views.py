f5d3b9baa7266f39b33f4b8beadac3cc3ac18f34
forksez <quad1812k@gmail.com>
2025-04-18 14:48:15 +0200

    SAVE2

--- users/views.py
+++ users/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
+from users.forms import UserLoginForm
+from django.contrib import auth
+from django.urls import reverse
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 def login(request):
-    context = {'title': 'Home - авторизация'}
+    
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('main:index'))
+    else:
+        form = UserLoginForm()
+
+    context = {
+        'title': 'Home - авторизация', 
+        'form': form
+    }
     return render(request, 'users/login.html', context)
 
 def registration(request):
